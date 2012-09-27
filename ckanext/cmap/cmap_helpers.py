63f0f0cde30bfbdfd95087619d4e8d2a83eea0ed
Sean Hammond <seanhammond@lavabit.com>
2012-09-27 15:44:27 +0200

    For popularity sort descending on first click
    i.e. show the most popular first no the least

--- ckanext/cmap/cmap_helpers.py
+++ ckanext/cmap/cmap_helpers.py
@@ -4,15 +4,19 @@ import ckan.lib.helpers as helpers
 
 def get_sort_by_url(order):
     queryvars = dict(plugins.toolkit.request.queryvars)
+    if order in ('views_recent',):
+        default = ' desc'
+    else:
+        default = ' asc'
     if 'sort' in queryvars:
         if queryvars['sort'] == order + ' asc':
             queryvars['sort'] = order + ' desc'
         elif queryvars['sort'] == order + ' desc':
             queryvars['sort'] = order + ' asc'
         else:
-            queryvars['sort'] = order + ' asc'
+            queryvars['sort'] = order + default
     else:
-        queryvars['sort'] = order + ' asc'
+        queryvars['sort'] = order + default
     if plugins.toolkit.c.id:
         queryvars['id'] = plugins.toolkit.c.id
     url = helpers.url_for(controller=plugins.toolkit.c.controller,
